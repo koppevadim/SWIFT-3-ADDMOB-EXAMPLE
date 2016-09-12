b03ad566ca5abbb328b0d9cfd60f912a67c051b5
Вадим Коппе <vadimkoppe@macbook-vadim.local>
2016-09-12 12:26:39 +0300

    Initial Commit

---
+++ AddMob/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AddMob
+//
+//  Created by Вадим Коппе on 12.09.16.
+//  Copyright © 2016 Вадим Коппе. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
